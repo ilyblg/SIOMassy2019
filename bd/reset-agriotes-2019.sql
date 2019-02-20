b8cc2da6cd34d5717ff4a6560cf51565be4cdf71
Miguel <pro.info.lmm@gmail.com>
2019-02-20 12:05:58 +0100

    Update 2 - organisation & verification : en cours => Projet

--- bd/reset-agriotes-2019.sql
+++ bd/reset-agriotes-2019.sql
@@ -70,8 +70,11 @@ BEGIN
     
 
 	INSERT INTO evenement(id_evenement, nom, date_effet) VALUES
-    (1, 'Remise des Diplome', date_effet - INTERVAL 1 YEAR),
-    (2, 'Réunion d''inscription', date_effet - INTERVAL 1 MONTH);
+    (1, 'Remise des Diplome', date_effet - INTERVAL 2 YEAR),
+    (2, 'Remise des Diplome', date_effet - INTERVAL 1 YEAR),
+    (3, 'Remise des Diplome', date_effet - INTERVAL 1 YEAR),
+    (4, 'Remise des Diplome', date_effet - INTERVAL 2 MONTH),
+    (5, 'Réunion d''inscription', date_effet - INTERVAL 1 MONTH);
     
 
     INSERT INTO formation(id_formation, nom, description) VALUES
@@ -81,86 +84,72 @@ BEGIN
 
 
 	INSERT INTO module(id_module, nom) VALUES
-		(1, 'Math'),
+	(1, 'Math'),
     (2, 'Réseau'),
-		(3, 'Bases de données');
+	(3, 'Bases de données'),
+    (4, 'Android'),
+    (5, 'Culture Générale'),
+    (6, 'Economie - droit');
 	
 
-	INSERT INTO personne(id_personne, nom, prenom, mail, tel, adresse, code_postal, ville, mot_de_passe) VALUES
-    (1,'Callahan','Quincy','metus@odioa.net','0960458869','Ap #649-4330 Suscipit Avenue','70651','Casablanca','gravida'),
-    (2,'Haley','Avye','vestibulum.Mauris@ornarefacilisiseget.com','0845694707','Ap #822-9800 Erat Rd.','48943','Greater Sudbury','ante'),
-    (3,'Sawyer','Venus','ligula@Nuncquis.com','0510602483','679-5855 Sem Avenue','35957','Acosse','faucibus'),
-    (4,'Stevenson','Eaton','augue@afeugiat.com','0546971033','5431 Mauris Road','12159','Kisi','pede'),
-    (5,'Silva','Price','vitae.risus@mollis.edu','0248369680','P.O. Box 897, 3249 Curabitur St.','11206','Moradabad','risus'),
-    (6,'Head','Briar','Integer.vulputate.risus@egetmetuseu.com','0387396561','Ap #937-519 Nunc Street','91300','Pınarbaşı','ultricies'),
-    (7,'Peck','Tyler','Cum@ligulaeu.com','0383484403','Ap #475-7358 Quis Rd.','68543','Reno','Sed'),
-    (8,'Montoya','Lester','Fusce.aliquam.enim@sitamet.edu','0282954239','9713 Molestie Street','12506','Sangerhausen','lorem'),
-    (9,'Figueroa','Christian','elit.Etiam@variusNamporttitor.org','0124629593','Ap #886-3467 Enim Rd.','02040','Calco','dictum'),
-    (10,'Wolfe','Burke','Cras.convallis.convallis@dapibusquamquis.com','0558645819','9053 Nam Road','65340','Santo Domingo','risus'),
-    (11,'Justice','Charles','at.pretium.aliquet@sapiencursusin.org','0531323259','7041 Mollis Av.','71884','Colchester','pede'),
-    (12,'Gardner','Kylynn','pretium.neque.Morbi@sit.org','0333847860','P.O. Box 252, 2276 Scelerisque St.','44330','Westmeerbeek','venenatis'),
-    (13,'Baker','Channing','taciti.sociosqu@Classaptenttaciti.edu','0771775852','P.O. Box 857, 3771 Proin Ave','44962','Bursa','risus'),
-    (14,'Baxter','Wallace','massa.lobortis@pede.edu','0342156271','654 Sit Av.','34850','Racine','sed'),
-    (15,'Kelly','Lyle','ac@eutempor.org','0303946453','972-3256 Purus, Rd.','50280','Vorst','ligula'),
-    (16,'Riddle','Doris','cubilia.Curae@purusac.net','0742542658','631-9977 Ac Street','87970','Herstappe','nisl'),
-    (17,'Sherman','Graiden','Duis.mi.enim@duinec.com','0300155826','P.O. Box 859, 3456 In Avenue','49070','Ibadan','tempor');
+	INSERT INTO personne(id_personne, nom, prenom, mail, tel, adresse, code_postal, ville, mot_de_passe, est_formateur, est_administrateur, date_inscription) VALUES
+    (1,'Théo','Moreau',         't,moreau@proboite.net',       '0691564808','82, Cours Marechal-Joffre','78180','MONTIGNY-LE-BRETONNEUX','condimentum', 0, 1,   date_effet - INTERVAL 1 YEAR + INTERVAL 2 DAY), # administrateur
+    (2,'Malik','Aubert',        'm.aubert@freelance.edu',      '0758143009','98, Avenue De Marlioz','95100','ARGENTEUIL','aubertfraterie', 1, 0,                date_effet - INTERVAL 7 MONTH + INTERVAL 8 DAY), # Formateur
+    (3,'Jérémy','Leclercq',     'j.leclercq@amasone.com',      '0610602483','84, avenue Voltaire','78600','MAISONS-LAFFITTE','motdepasserigoureux', 0, 0,       date_effet - INTERVAL 6 MONTH + INTERVAL 15 DAY), # formateur
+    (4,'Mirabelle','Huppé',     'm.huppé@gougleu.com',         '0646971033','56, rue Jean-Monnet','95190','GOUSSAINVILLE','gousshupp', 0, 0,                    date_effet - INTERVAL 6 MONTH + INTERVAL 3 DAY),
+    (5,'Silvia','Gonzalez',     's.gonzalez@gmale.edu',        '0648369680','53, rue du Faubourg National','95150','TAVERNY','rictusmaloficus', 0, 0,           date_effet - INTERVAL 6 MONTH + INTERVAL 1 DAY),
+    (6,'Dylan','Briar',         'd.briar@classprepa.edu',      '0187396561','52, boulevard Bryas','92400','COURBEVOIE','12340000', 0, 0,                        date_effet - INTERVAL 6 MONTH + INTERVAL 3 DAY),
+    (7,'Alexandre','Morel',     'a.lexandre@gougleu.com',      '0783484403','56, rue des Soeurs','LA CELLE-SAINT-CLOUD','Sed', 1, 0,                            date_effet - INTERVAL 5 MONTH + INTERVAL 8 DAY), # formateur
+    (8,'Montoya','Lester',      'm.lester@amasone.com',        '0182954239','73, rue du Château','78100','SAINT-GERMAIN-EN-LAYE','lorem', 0, 0,                 date_effet - INTERVAL 4 MONTH + INTERVAL 4 DAY),
+    (9,'Figueroa','Christian',  'f.christian@gmale.fr',        '0114629593','90, rue du Président Roosevelt','91600','SAVIGNY-SUR-ORGE ','dictum', 0, 0,        date_effet - INTERVAL 4 MONTH + INTERVAL 2 DAY),
+    (10,'Marwane','Roche',      'm.roche@gmale.com',           '0128645819','71, rue La Boétie','75015','PARIS','risus', 0, 0,                                  date_effet - INTERVAL 4 MONTH + INTERVAL 2 DAY),
+    (11,'Justice','Charles',    'j.charles@freelance.edu',     '0131323259','35, rue Lenotre','93240','STAINS','pede', 0, 0,                                    date_effet - INTERVAL 4 MONTH + INTERVAL 1 DAY), # formateur
+    (12,'Nicolas','Colin',      'n.colin@formation.net',       '0133847860','33, rue de Groussay','93230','ROMAINVILLE','venenatis', 0, 1,                      date_effet - INTERVAL 3 MONTH + INTERVAL 1 DAY),
+    (13,'Didier','Vasseur',     'd.vasseur@classprepa.edu',    '0771775852','18, rue Victor Hugo','91940','LES ULIS','risus', 1, 0,                             date_effet - INTERVAL 2 MONTH + INTERVAL 3 DAY),
+    (14,'Stephanie','Platano',  's.platano@proboite.net',      '0642156271','88, rue du Fossé des Tanneurs','75008','PARIS','sed', 1, 1,                        date_effet - INTERVAL 2 MONTH + INTERVAL 4 DAY), # formateur & admin
+    (15,'Brigitte','Masson',    'b.masson@gougleu.com',        '0103946453','15, rue du Clair Bocage','75013','PARIS','ligula', 0, 0,                           date_effet - INTERVAL 2 MONTH + INTERVAL 2 DAY),
+    (16,'Othmane','Fernandez',  'o.fernandez@gmale.net',       '0742542658','23, rue des six frères Ruellan','75018','PARIS','nisl', 0, 0,                      date_effet - INTERVAL 2 MONTH + INTERVAL 1 DAY),
+    (17,'Samuel','Tisserand', 's.tisserand@formation.net',     '0645507077','86, Place Napoléon','59130','LAMBERSART','passwordsecured', 0, 1,                  date_effet - INTERVAL 1 MONTH); # administrateur
     
-    INSERT INTO echange(id_echange, id_personne, id_type_echange, instant, texte) VALUES
-    (1, 2, 3, date_effet - INTERVAL 2 YEAR + INTERVAL 5 MONTH, 'Demande d''information concernant la formation'),
-    (2, 2, 1, date_effet - INTERVAL 2 YEAR + INTERVAL 7 MONTH, 'Demande d''inscription'),
-    (3, 2, 2, date_effet - INTERVAL 2 YEAR + INTERVAL 7 MONTH, 'Confirmation de l''invitation a la journée d''information'),
-    (4, 22, 1, date_effet - INTERVAL 2 YEAR + INTERVAL 7 MONTH + INTERVAL 1 DAY, 'Contact 1'),
-    (5, 22, 2, date_effet - INTERVAL 2 YEAR + INTERVAL 7 MONTH + INTERVAL 3 DAY, 'Contact 2'),
-    (6, 22, 3, date_effet - INTERVAL 2 YEAR + INTERVAL 7 MONTH + INTERVAL 5 DAY, 'Contact 3'),
-    (7, 22, 4, date_effet - INTERVAL 2 YEAR + INTERVAL 8 MONTH, 'Contact 4'),
-    (8, 22, 3, date_effet - INTERVAL 2 YEAR + INTERVAL 8 MONTH + INTERVAL 1 DAY, 'Contact 5'),
-    (9, 2, 3, date_effet - INTERVAL 2 DAY, 'Demande d''information'),
-    (10, 2, 3, date_effet, 'Demande d''annulation d''inscription');
+    INSERT INTO echange(id_echange, id_personne, instant, texte) VALUES
+    (1, 2, date_effet - INTERVAL 2 YEAR + INTERVAL 5 MONTH, 'Demande d''information concernant la formation'),
+    (2, 2, date_effet - INTERVAL 2 YEAR + INTERVAL 7 MONTH, 'Demande d''inscription'),
+    (3, 2, date_effet - INTERVAL 2 YEAR + INTERVAL 7 MONTH, 'Confirmation de l''invitation a la journée d''information'),
+    (4, 22, date_effet - INTERVAL 2 YEAR + INTERVAL 7 MONTH + INTERVAL 1 DAY, 'Contact 1'),
+    (5, 22, date_effet - INTERVAL 2 YEAR + INTERVAL 7 MONTH + INTERVAL 3 DAY, 'Contact 2'),
+    (6, 22, date_effet - INTERVAL 2 YEAR + INTERVAL 7 MONTH + INTERVAL 5 DAY, 'Contact 3'),
+    (7, 22, date_effet - INTERVAL 2 YEAR + INTERVAL 8 MONTH, 'Contact 4'),
+    (8, 22, date_effet - INTERVAL 2 YEAR + INTERVAL 8 MONTH + INTERVAL 1 DAY, 'Contact 5'),
+    (9, 2, date_effet - INTERVAL 2 DAY, 'Demande d''information'),
+    (10, 2, date_effet, 'Demande d''annulation d''inscription');
    
     INSERT INTO invitation(id_personne, id_evenement) VALUES
-    (1, 1),
-    (2, 1),
-    (2, 2),
-    (22, 2);
+    (3, 1),
+    (8, 1),
+    (13, 2),
+    (16, 2);
 
     INSERT INTO qcm(id_qcm, date_creation, titre, id_formateur) VALUES
-    (1, date_effet, '', 23),
-    (2, date_effet, '', 25),
-    (3, date_effet, '', 24),
-    (4, date_effet, '', 23);
+    (1, date_effet, 'Les pratiques utils', 7),
+    (2, date_effet, 'Les questions sans questionnements', 2),
+    (3, date_effet, 'Pratiques de la véracités des propos', 24),
+    (4, date_effet, 'Test de langue parlementaire', 13);
 
-    INSERT INTO session_formation(id_session_formation, id_formation, date_debut, date_fin, est_ouverte) VALUES
-    (1, 1, date_effet - INTERVAL 2 YEAR, date_effet - INTERVAL 1 YEAR, false), -- passé
-    (2, 1, date_effet - INTERVAL 8 MONTH, date_effet + INTERVAL 6 MONTH, false), -- en cours
-    (3, 2, date_effet - INTERVAL 2 MONTH, date_effet + INTERVAL 1 YEAR, false), -- en cours
-    (4, 2, date_effet + INTERVAL 6 MONTH, date_effet + INTERVAL 2 YEAR, true); -- ouverte a candidature
+    INSERT INTO session_formation(id_session_formation, id_formation, date_debut, date_fin) VALUES
+    (1, 1, date_effet - INTERVAL 2 YEAR, date_effet - INTERVAL 1 YEAR), -- passé
+    (2, 2, date_effet - INTERVAL 8 MONTH, date_effet + INTERVAL 6 MONTH), -- en cours
+    (3, 1, date_effet - INTERVAL 2 MONTH, date_effet + INTERVAL 1 YEAR), -- en cours
+    (4, 2, date_effet + INTERVAL 6 MONTH, date_effet + INTERVAL 2 YEAR); -- ouverte a candidature
     
     INSERT INTO avis(id_personne, id_questionnaire, commentaire, note, date_avis) VALUES
-    (2,  1, 'Commentaire inutile', 6,  date_effet - INTERVAL 2 YEAR - INTERVAL 2 MONTH),
-    (1,  3, 'Commentaire inutile', 12, date_effet - INTERVAL 2 YEAR - INTERVAL 2 MONTH),
-    (16, 2, 'Commentaire inutile', 3,  date_effet - INTERVAL 2 YEAR - INTERVAL 2 MONTH),
-    (8,  4, 'Commentaire inutile', 16, date_effet - INTERVAL 2 YEAR - INTERVAL 2 MONTH),
-    (12, 1, 'Commentaire inutile', 7,  date_effet - INTERVAL 2 YEAR - INTERVAL 2 MONTH),
-    (1,  3, 'Commentaire inutile', 14, date_effet - INTERVAL 2 YEAR - INTERVAL 2 MONTH),
-    (10, 2, 'Commentaire inutile', 8,  date_effet - INTERVAL 2 YEAR - INTERVAL 2 MONTH),
-    (6,  4, 'Commentaire inutile', 10, date_effet - INTERVAL 2 YEAR - INTERVAL 2 MONTH),
-
-    INSERT INTO evaluation(id_evaluation, id_session_formation, id_module, id_formateur, date_debut, nb_minutes, titre) VALUES
-    (1, 1, 1, 23, date_effet - INTERVAL 1 YEAR - INTERVAL 4 MONTH, 120, 'Evaluation finale de mathématique'),
-    (2, 1, 2, 24, date_effet - INTERVAL 1 YEAR - INTERVAL 4 MONTH, 240, 'Evaluation mise en place d''un serveur LAMP'),
-    (3, 2, 2, 24, date_effet, 60, 'Evaluation réseau 2'),
-    (4, 3, 2, 24, date_effet + INTERVAL 4 MONTH, 60, 'Evaluation réseau 3'),
-    (5, 3, 1, 23, date_effet, 60, 'Math 2'),
-    (6, 3, 1, 23, date_effet + INTERVAL 4 MONTH, 120, 'Math 3');
-
-
-
-
-
-
-
-
-
+    (2,  1, 'Commentaire inutile', 6,  date_effet - INTERVAL 2 YEAR - INTERVAL 5 MONTH),
+    (1,  3, 'Commentaire inutile', 12, date_effet - INTERVAL 2 YEAR - INTERVAL 4 MONTH),
+    (16, 2, 'Commentaire inutile', 3,  date_effet - INTERVAL 1 YEAR - INTERVAL 2 MONTH),
+    (8,  4, 'Commentaire inutile', 16, date_effet - INTERVAL 1 YEAR - INTERVAL 2 MONTH),
+    (12, 1, 'Commentaire inutile', 7,  date_effet - INTERVAL 1 YEAR - INTERVAL 1 MONTH),
+    (1,  3, 'Commentaire inutile', 14, date_effet - INTERVAL 8 MONTH - INTERVAL 5 DAY),
+    (10, 2, 'Commentaire inutile', 8,  date_effet - INTERVAL 6 MONTH - INTERVAL 2 DAY),
+    (6,  4, 'Commentaire inutile', 10, date_effet - INTERVAL 4 MONTH - INTERVAL 1 DAY),
 
     INSERT INTO candidature(id_personne, id_session_formation, id_etat_candidature, date_effet) VALUES
     (1, 1, 6, date_effet - INTERVAL 2 YEAR - INTERVAL 2 MONTH),
@@ -176,23 +165,50 @@ BEGIN
     (11, 2, 6, date_effet - INTERVAL 8 MONTH - INTERVAL 2 MONTH + INTERVAL 12 DAY),
     (12, 2, 3, date_effet - INTERVAL 8 MONTH - INTERVAL 2 MONTH + INTERVAL 23 DAY),
     (13, 3, 6, date_effet - INTERVAL 2 MONTH - INTERVAL 2 MONTH + INTERVAL 9 DAY),
-    (14, 3, 6, date_effet - INTERVAL 2 MONTH - INTERVAL 2 MONTH + INTERVAL 12 DAY),
+    (14, 3, 7, date_effet - INTERVAL 2 MONTH - INTERVAL 2 MONTH + INTERVAL 12 DAY),
     (15, 3, 6, date_effet - INTERVAL 2 MONTH - INTERVAL 2 MONTH + INTERVAL 19 DAY),
-    (16, 3, 6, date_effet - INTERVAL 2 MONTH - INTERVAL 2 MONTH + INTERVAL 23 DAY),
+    (16, 3, 7, date_effet - INTERVAL 2 MONTH - INTERVAL 2 MONTH + INTERVAL 23 DAY),
     (17, 3, 3, date_effet - INTERVAL 2 MONTH - INTERVAL 2 MONTH + INTERVAL 28 DAY),
     (18, 4, 6, date_effet - INTERVAL 1 MONTH + INTERVAL 2 DAY),
     (19, 4, 6, date_effet - INTERVAL 1 MONTH + INTERVAL 5 DAY),
     (20, 4, 6, date_effet - INTERVAL 1 MONTH + INTERVAL 8 DAY),
-    (21, 4, 4, date_effet - INTERVAL 1 MONTH + INTERVAL 12 DAY),
-    (22, 4, 3, date_effet - INTERVAL 1 MONTH + INTERVAL 16 DAY),
-    (1, 2, 6, date_effet - INTERVAL 8 MONTH - INTERVAL 2 MONTH + INTERVAL 28 DAY),
-    (2, 4, 1, date_effet - INTERVAL 1 MONTH + INTERVAL 26 DAY);
-    
-    INSERT INTO formateur(id_formateur) VALUES
-    (23),
-    (24),
-    (25);
-    
+    (21, 4, 2, date_effet - INTERVAL 1 MONTH + INTERVAL 12 DAY),
+    (22, 4, 3, date_effet - INTERVAL 1 MONTH + INTERVAL 16 DAY);
+
+    INSERT INTO evaluation(id_evaluation, id_session_formation, id_module, id_formateur, date_debut, nb_minutes, titre) VALUES
+    (1, 1, 1, 23, date_effet - INTERVAL 1 YEAR - INTERVAL 4 MONTH, 120, 'Evaluation finale de mathématique'),
+    (2, 1, 2, 24, date_effet - INTERVAL 1 YEAR - INTERVAL 4 MONTH, 240, 'Evaluation mise en place d''un serveur LAMP'),
+    (3, 2, 2, 24, date_effet, 60, 'Evaluation réseau 2'),
+    (4, 3, 2, 24, date_effet + INTERVAL 4 MONTH, 60, 'Evaluation réseau 3'),
+    (5, 3, 1, 23, date_effet, 60, 'Math 2'),
+    (6, 3, 1, 23, date_effet + INTERVAL 4 MONTH, 120, 'Math 3');
+
+    INSERT INTO passage_qcm(id_passage_qcm, date_passage, id_qcm, id_personne) VALUES
+    (1, date_effet - INTERVAL 1 YEAR - INTERVAL 3 MONTH, 1, 4),
+    (2, date_effet - INTERVAL 1 YEAR - INTERVAL 4 MONTH, 1, 3),
+    (3, date_effet, 2, 6),
+    (4, date_effet, 2, 8),
+    (5, date_effet, 2, 10),
+    (6, date_effet + INTERVAL 3 MONTH, 3, 5),
+    (7, date_effet + INTERVAL 3 MONTH, 3, 9),
+    (8, date_effet + INTERVAL 4 MONTH, 4, 16),
+
+    INSERT INTO projet(id_projet, id_formateur, id_session_formation, titre, date_creation, date_butoir) VALUES
+    (1, 23, 1, 'Maintient d''un site web', date_effet - INTERVAL 1 YEAR - INTERVAL 3 MONTH, date_effet - INTERVAL 1 YEAR - INTERVAL 3 MONTH),
+    (2, 24, 1, 'Calcule de matrice', date_effet - INTERVAL 1 YEAR - INTERVAL 3 MONTH, date_effet - INTERVAL 1 YEAR - INTERVAL 3 MONTH),
+    (3, 23, 1, 'Maintient d''un site web', date_effet - INTERVAL 1 YEAR - INTERVAL 3 MONTH, date_effet - INTERVAL 1 YEAR - INTERVAL 3 MONTH),
+    (4, 23, 1, 'Maintient d''un site web', date_effet - INTERVAL 1 YEAR - INTERVAL 3 MONTH, date_effet - INTERVAL 1 YEAR - INTERVAL 3 MONTH),
+    (5, 23, 1, 'Maintient d''un site web', date_effet - INTERVAL 1 YEAR - INTERVAL 3 MONTH, date_effet - INTERVAL 1 YEAR - INTERVAL 3 MONTH),
+    (6, 23, 1, 'Maintient d''un site web', date_effet - INTERVAL 1 YEAR - INTERVAL 3 MONTH, date_effet - INTERVAL 1 YEAR - INTERVAL 3 MONTH);
+
+
+
+    # =================================================================================================================================
+    #  TOP = OK, BOTTOM = NOT IMPLEMENTED YET
+    # =================================================================================================================================
+
+
+
     INSERT INTO seance(id_seance, id_session_formation, id_formateur, id_module, jour, creneau) VALUES
     (1, 1, 23, 1, date_effet - INTERVAL 2 YEAR + INTERVAL 4 MONTH, 1),
     (2, 2, 23, 1, date_effet - INTERVAL 2 YEAR + INTERVAL 4 MONTH, 2),
@@ -265,9 +281,7 @@ BEGIN
         (8, 1), (8, 2)
 	);
    
-    INSERT INTO projet(id_projet, id_formateur, id_session_formation, titre) VALUES
-    (1, 23, 1, 'Maintient d''un site web'),
-    (2, 24, 1, 'Calcule de matrice');
+
     
     INSERT INTO type_echange(id_type_echange, libelle) VALUES
     (1, 'Email reçu'),
