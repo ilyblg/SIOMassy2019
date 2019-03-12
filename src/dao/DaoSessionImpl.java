db7e4264dba7a3dc39a26543409de0da75418123
florianlouis <florianlouis.1994@hotmail.com>
2019-03-12 16:28:01 +0100

    UPDATE dao

--- src/dao/DaoSessionImpl.java
+++ src/dao/DaoSessionImpl.java
@@ -2,21 +2,18 @@ package dao;
 
 import entity.Personne;
 
-public class DaoSessionImpl implements DaoSession{
+public class DaoSessionImpl{
 
-	@Override
 	public void inscription(Personne personne) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void seConnecter(String login, String mdp) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void changerMdp(String login, String mdp, String mail) {
 		// TODO Auto-generated method stub
 		
